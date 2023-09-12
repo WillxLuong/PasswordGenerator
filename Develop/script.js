2bc8f172ef6a3c960ee19996eaafdc8fa1a01b53
William <toanluong162@gmail.com>
2023-09-12 15:23:02 +1000

    adding random function and sets of characters

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,22 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+// Declaring all the characters that can be used 
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChars = "0123456789";
+var specialChars = "!@#$%^&*()_-+=<>?";
+
+// Function used to randomise passwords
+function getRandomCharacter(charSet) {
+  var randomIndex = Math.floor(Math.random() * charSet.length);
+  return charSet.charAt(randomIndex);
+}
+
+function generatePassword() {
+  var passwordLength = prompt("Enter password length (between 8 and 128 characters):");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
