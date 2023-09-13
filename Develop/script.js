d8e7da4f2e72f2c476366a67aaa116f18c8b9c60
William <toanluong162@gmail.com>
2023-09-13 10:50:19 +1000

    changes to js and html to fit acceptance criteria and make the website more user friendly

--- Develop/script.js
+++ Develop/script.js
@@ -1,51 +1,72 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Get references to the #generate element and checkboxes
 var generateBtn = document.querySelector("#generate");
+var passwordLengthInput = document.querySelector("#password-length");
+var includeLowercaseCheckbox = document.querySelector("#include-lowercase");
+var includeUppercaseCheckbox = document.querySelector("#include-uppercase");
+var includeNumericCheckbox = document.querySelector("#include-numeric");
+var includeSpecialCheckbox = document.querySelector("#include-special");
 
-
-// Declaring all the characters that can be used 
+// Character sets for different types of characters
 var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericChars = "0123456789";
 var specialChars = "!@#$%^&*()_-+=<>?";
 
-// Function used to randomise passwords
+// Function to generate a random character from a given character set
 function getRandomCharacter(charSet) {
   var randomIndex = Math.floor(Math.random() * charSet.length);
   return charSet.charAt(randomIndex);
 }
 
-// Validate password between 8-128 characters
+// Function to generate a password based on user criteria
 function generatePassword() {
-  var passwordLength = prompt("Enter password length (between 8 and 128 characters):");
-  
-  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+  var passwordLength = parseInt(passwordLengthInput.value);
+
+  // Validate the password length
+  if (!isNaN(passwordLength) && passwordLength >= 8 && passwordLength <= 128) {
+    // Create a character set based on user criteria
+    var charSet = "";
+    if (includeLowercaseCheckbox.checked) {
+      charSet += lowercaseChars;
+    }
+    if (includeUppercaseCheckbox.checked) {
+      charSet += uppercaseChars;
+    }
+    if (includeNumericCheckbox.checked) {
+      charSet += numericChars;
+    }
+    if (includeSpecialCheckbox.checked) {
+      charSet += specialChars;
+    }
+
+    // Check if at least one character type is selected
+    if (charSet === "") {
+      alert("Please select at least one character type.");
+      return "";
+    }
+
+    // Generate the password
+    var password = "";
+    for (var i = 0; i < passwordLength; i++) {
+      password += getRandomCharacter(charSet);
+    }
+
+    return password;
+  } else {
     alert("Please enter a valid password length between 8 and 128 characters.");
     return "";
   }
 }
 
-// Variables for the of characters
-var includeLowercase = confirm("Include lowercase characters?");
-var includeUppercase = confirm("Include uppercase characters?");
-var includeNumeric = confirm("Include numeric characters?");
-var includeSpecial = confirm("Include special characters?");
-
- // Making sure that at least 1 of the charcter types are selected
- if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
-  alert("Please select at least one character type.");
-  return "";
-}
-
-// Write password to the #password input
+// Function to write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  if (password !== "") {
+    passwordText.value = password;
+    alert("Generated Password: " + password);
+  }
 }
 
 // Add event listener to generate button
